53141893b13d3c6f371808986a757e8510095989
Yidi <41704405+pothie@users.noreply.github.com>
2020-08-21 13:33:00 -0400

    Create P1: Multiples of 3 and 5.py

---
+++ P1: Multiples of 3 and 5.py
@@ -0,0 +1,13 @@
+#If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+#Find the sum of all the multiples of 3 or 5 below 1000.
+
+import sys
+
+t = int(input().strip())
+for i in range(t):
+    n = int(input().strip())
+    m3 = (((n-1)//3)*((n-1)//3+1))//2*3
+    m5 = (((n-1)//5)*((n-1)//5+1))//2*5
+    m15 = (((n-1)//15)*((n-1)//15+1))//2*15
+    print(m3+m5-m15)
