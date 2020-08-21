a9fb69602018cff0de0885dde2e613974e7233d1
Yidi <41704405+pothie@users.noreply.github.com>
2020-08-21 13:37:49 -0400

    Update P1: Multiples of 3 and 5.py

--- P1: Multiples of 3 and 5.py
+++ P1: Multiples of 3 and 5.py
@@ -1,6 +1,6 @@
-#If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
-#Find the sum of all the multiples of 3 or 5 below 1000.
+# Find the sum of all the multiples of 3 or 5 below 1000.
 
 import sys
 
@@ -11,3 +11,6 @@ for i in range(t):
     m5 = (((n-1)//5)*((n-1)//5+1))//2*5
     m15 = (((n-1)//15)*((n-1)//15+1))//2*15
     print(m3+m5-m15)
+
+# Arithmic operation saves time compared to looping through all numbers lesss than t.
+# //(integer division) here saves a lot of time compared to (/) float division. 
